ddaeaae663fae949cefa770844169ddf2337cafa
Claymore-AI <dimalishchuk2003@gmail.com>
2025-02-25 21:35:44 +0200

    fix mistake

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class Cargo:
 class BaseRobot:
     def __init__(self, name: str,
                  weight: int,
-                 coords: list = None) -> None:
+                 coords: list[int] | None = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords
@@ -33,8 +33,8 @@ class BaseRobot:
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str,
                  weight: int,
-                 coords: list = None) -> None:
-        super().__init__(name, weight,)
+                 coords: list[int] | None = None) -> None:
+        super().__init__(name, weight)
 
         self.coords = coords
         if self.coords is None:
@@ -51,12 +51,14 @@ class DeliveryDrone(FlyingRobot):
     def __init__(self, name: str,
                  weight: int,
                  max_load_weight: int,
-                 current_load: int,
-                 coords: list = None) -> None:
-        self.max_load_weight = max_load_weight
-        self.current_load = current_load
-
+                 current_load: Cargo = None,
+                 coords: list[int] | None = None,) -> None:
         super().__init__(name, weight, coords)
+
+        self.max_load_weight = max_load_weight
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
